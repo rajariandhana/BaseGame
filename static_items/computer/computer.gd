6b8d0d4d4693b48e7b7da6d55c0e468dfdf83323
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2026-04-14 22:33:45 +1000

    feat: computer talk and table plant

---
+++ static_items/computer/computer.gd
@@ -0,0 +1,18 @@
+extends Talkable
+var line_3: String
+
+func _ready() -> void:
+	super()
+	line_3 = get_line(3)
+
+func respond(index: int, answer: String) -> int:
+	print("index: ",index," answer: ",answer)
+	match index:
+			2:
+				var new_line: String = get_line(index+1) + answer+"?"
+				set_line(index+1, new_line)
+			3:
+				if answer == "No":
+					set_line(3, line_3)
+					return 2
+	return index + 1
