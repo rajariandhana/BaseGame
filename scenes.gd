0b25fc3d697f0f659741c9feda6da4b6f77e1a50
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2026-05-17 22:25:18 +1000

    feat: walls and roof

--- scenes.gd
+++ scenes.gd
@@ -4,7 +4,7 @@ const main_menu_scene: String = "res://UI/main_menu/main_menu.tscn"
 const room_0_scene: String = "res://rooms/room_0.tscn"
 const doors_scene: String = "res://game.tscn"
 
-const level_0_scene: String = "res://levels/level_0.tscn"
+const level_0_scene: String ="res://levels/level_0/level_0.tscn"
 
 func _ready() -> void:
 	pass
